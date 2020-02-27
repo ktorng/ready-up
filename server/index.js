3ee84180eeedc470f1b8d9c67ddd5d78dcda9eb3
Kenneth Torng <ktorng@gmail.com>
2020-02-27 10:00:28 -0600

    Restructure and set up basic schema and server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({
+    typeDefs
+});
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
---
+++ server/schema.js
@@ -0,0 +1,55 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Game {
+        id: ID!
+        accessCode: String!
+        status: GameStatus
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        game: Game
+        userStatus: UserStatus
+    }
+
+    type Query {
+        users(gameId: ID!): [User]
+        game(accessCode: String!): Game
+        me: User
+    }
+
+    type Mutation {
+        createGame(userId: ID!): GameUpdateResponse!
+        updateGame(gameId: ID!, status: GameStatus): GameUpdateResponse!
+        deleteGame(gameId: ID!): GameUpdateResponse!
+        updateUser(userId: ID!, status: UserStatus, gameId: ID): UserUpdateResponse!
+        login(email: String): String # login token
+    }
+
+    type GameUpdateResponse {
+        success: Boolean!
+        message: String
+        game: Game
+    }
+
+    type UserUpdateResponse {
+        success: Boolean!
+        message: String
+        user: User
+    }
+
+    enum GameStatus {
+        WAITING
+        IN_PROGRESS
+        COMPLETED
+    }
+
+    enum UserStatus {
+        WAITING
+        READY
+    }
+`;
+
+module.exports = typeDefs;
