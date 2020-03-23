6448f38a4be26cf5d92193fe9ffc08f3ca064fd6
Kenneth Torng <ktorng@gmail.com>
2020-03-22 21:17:20 -0500

    Add generateMission util

--- server/src/utils/crew.js
+++ server/src/utils/crew.js
@@ -1,3 +1,7 @@
+const colors = ['R', 'G', 'B', 'Y', 'W'];
+const numbers = [...Array(9)].map((_, i) => i + 1);
+const rockets = [...Array(4)].map((_, i) => i + 1);
+
 module.exports = {
     /**
      * Generates a list of player objects which contain a list of cards and whether or not the
@@ -6,14 +10,14 @@ module.exports = {
      * @returns {{isCommander: boolean, hand: []}[]}
      */
     generateGame: () => {
-        const colors = ['R', 'G', 'B', 'Y', 'W'];
-        const numbers = [...Array(9)].map((_, i) => i + 1);
-        const rockets = [...Array(4)].map((_, i) => i + 1);
         const players = [...Array(4)].map(() => ({ isCommander: false, hand: [] }));
-        const cards = colors.reduce((tot, color) => [
-            ...tot,
-            ...(color === 'W' ? rockets : numbers).map(number => ({ color, number }))
-        ], []);
+        const cards = colors.reduce(
+            (tot, color) => [
+                ...tot,
+                ...(color === 'W' ? rockets : numbers).map((number) => ({ color, number }))
+            ],
+            []
+        );
 
         while (cards.length) {
             // random index and player index
@@ -35,4 +39,45 @@ module.exports = {
 
         return players;
     },
+    /**
+     * Generates a list of task objects which contain card and optional order
+     *
+     * @param count - number of tasks
+     * @param taskReqs - task requirements; object of counts for each requirement
+     */
+    generateMission: (count, taskReqs) => {
+        const tasks = {
+            unordered: [],
+            ordered: [],
+            first: null,
+            last: null,
+        };
+        const cards = colors
+            .filter((color) => color !== 'W')
+            .reduce((tot, color) => [...tot, ...numbers.map((number) => ({ color, number }))], []);
+
+        [...Array(count)].forEach(() => {
+            // get random card
+            const i = Math.floor(Math.random() * cards.length);
+            const temp = cards[i];
+            cards[i] = cards[cards.length - 1];
+            cards[cards.length - 1] = temp;
+            const card = cards.pop();
+
+            // push to task type depending on task requirements
+            if (taskReqs.first) {
+                tasks.first = card;
+                taskReqs.first = 0;
+            } else if (taskReqs.last) {
+                tasks.last = card;
+                taskReqs.last = 0;
+            } else if (taskReqs.ordered) {
+                tasks.ordered.push(card);
+            } else {
+                tasks.unordered.push(card);
+            }
+        });
+
+        return tasks;
+    }
 };
