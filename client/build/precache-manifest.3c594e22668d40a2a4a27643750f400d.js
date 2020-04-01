7d65c512ec85518a9d0e3d462e6322afea79d9cf
Kenneth Torng <ktorng@gmail.com>
2020-04-01 10:44:11 -0500

    Add  to source control

---
+++ client/build/precache-manifest.3c594e22668d40a2a4a27643750f400d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cf81a56058146ddd5e02e296da0142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1057046b67122d8374b0",
+    "url": "/static/css/main.1f3379d3.chunk.css"
+  },
+  {
+    "revision": "19d396027d8bb40134f4",
+    "url": "/static/js/2.b272d126.chunk.js"
+  },
+  {
+    "revision": "b1800d7827abf32b11673bedb24b4cc2",
+    "url": "/static/js/2.b272d126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1057046b67122d8374b0",
+    "url": "/static/js/main.7a85ed65.chunk.js"
+  },
+  {
+    "revision": "59f0c22ded09b8410d8f",
+    "url": "/static/js/runtime-main.0ef212b5.js"
+  }
+]);
